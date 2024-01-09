c4b392e957f4868afb508d6ad7728af70db3c7f2
jiten <saphalrai53@gmail.com>
2024-01-09 20:33:42 +0545

    improve

--- lib/widgets/cal_button.dart
+++ lib/widgets/cal_button.dart
@@ -1,19 +1,26 @@
 import 'package:calculator_app/constant/colors.dart';
+import 'package:calculator_app/provider/cal_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CalculateButton extends StatelessWidget {
   const CalculateButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      height: 160,
-      width:70,
-      decoration: BoxDecoration(
-        color: AppColors.secondaryColor,
-        borderRadius: BorderRadius.circular(40),),
-      child: Center(child: Text("=",style: TextStyle(fontSize:32),)
-      ) ,
+    return  InkWell(
+      onTap: (){
+        Provider.of<CalculatorProvider>(context, listen:false).setValue("=");
+      },
+      child: Container(
+        height: 160,
+        width:70,
+        decoration: BoxDecoration(
+          color: AppColors.secondaryColor,
+          borderRadius: BorderRadius.circular(40),),
+        child: Center(child: Text("=",style: TextStyle(fontSize:32),)
+        ) ,
+      ),
     );
   }
 }
